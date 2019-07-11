a71001f4a0e8dbd6e5fd0b9d4437387a2af85a5a
Jordynn Otto <j_taylor92@yahoo.com>
2019-07-10 19:30:45 -0700

    Updates

---
+++ precache-manifest.ad5d51146f5a44cc90e20d4cc5c87096.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a303705a742a09e4ab3f7d4d3a9034b4",
+    "url": "/Clicky-game/index.html"
+  },
+  {
+    "revision": "47bcb32734eeed19d5ef",
+    "url": "/Clicky-game/static/css/main.6f11713a.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "47bcb32734eeed19d5ef",
+    "url": "/Clicky-game/static/js/main.3eac405c.chunk.js"
+  },
+  {
+    "revision": "cda0924bd23e3d9029fa",
+    "url": "/Clicky-game/static/js/runtime~main.952307d2.js"
+  }
+]);
